997632b39ee24af71ae12954cc42de6abea4edaf
Dylan Morris <dylanrobertmorris@gmail.com>
2021-05-17 12:08:35 -0500

    fixed parameters in models

--- rareapi/models/posttags.py
+++ rareapi/models/posttags.py
@@ -1,6 +1,5 @@
 from django.db import models
 
-class PostTag():
+class PostTag(models.Model):
     tag = models.ForeignKey("Tag", on_delete=models.CASCADE)
     post = models.ForeignKey("Post", on_delete=models.CASCADE)
-    
