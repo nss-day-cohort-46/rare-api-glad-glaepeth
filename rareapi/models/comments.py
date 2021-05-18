37825fce3d3e444859d24bb08e81b566c94b7146
Ccourter11 <courtercalvin@yahoo.com>
2021-05-18 15:33:18 -0500

    fix: deleted db

--- rareapi/models/comments.py
+++ rareapi/models/comments.py
@@ -6,7 +6,7 @@ class Comment(models.Model):
     content = models.CharField(max_length=500)
     created_on = models.DateTimeField()
     post = models.ForeignKey("Post", on_delete=models.CASCADE)
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    author = models.ForeignKey("RareUser", on_delete=models.CASCADE)
 
 
 
