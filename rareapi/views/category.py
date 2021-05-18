3e0eca9a15a9b1a7ae01a7278628ec654abf181a
Ccourter11 <courtercalvin@yahoo.com>
2021-05-18 13:26:15 -0500

    feat: editing categories

--- rareapi/views/category.py
+++ rareapi/views/category.py
@@ -24,9 +24,33 @@ class CategoryView(ViewSet):
             categories, many=True, context={'request': request})
         return Response(serializer.data)
 
+
+    def update(self, request, pk=None):
+
+
+        category = Category.objects.get(pk=pk)
+        category.label = request.data["label"]
+        category.save()
+
+        return Response({}, status=status.HTTP_204_NO_CONTENT)
+    
+    def delete(self, request, pk=None):
+
+        try:
+            category = Category.objects.get(pk=pk)
+            category.delete()
+
+            return Response({}, status=status.HTTP_204_NO_CONTENT)
+
+        except Category.DoesNotExist as ex:
+            return Response({'message': ex.args[0]}, status=status.HTTP_404_NOT_FOUND)
+
+        except Exception as ex:
+            return Response({'message': ex.args[0]}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        
 class CategorySerializer(serializers.ModelSerializer):
 # how we build the object
 
     class Meta:
         model = Category
-        fields = ('id', 'label')
+        fields = ('id', 'label')        
