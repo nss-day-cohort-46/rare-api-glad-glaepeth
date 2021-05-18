348157ff772ef0cb3d63a7204b3c2ed114ba2b7b
Caleb James <calebsjames@gmail.com>
2021-05-18 13:20:01 -0500

    build: CommentView can retrieve, list and delete

---
+++ rareapi/views/comment.py
@@ -0,0 +1,79 @@
+from django.core.exceptions import ValidationError
+from rest_framework import status
+from django.http import HttpResponseServerError
+from rest_framework.viewsets import ViewSet
+from rest_framework.response import Response
+from rest_framework import serializers
+from rest_framework import status
+from rareapi.models import Comment
+from django.contrib.auth.models import User
+
+
+class CommentView(ViewSet):
+
+    # Get a single record
+    def retrieve(self, request, pk=None):
+        try:
+            comment = Comment.objects.get(pk=pk)
+            # categories = Category.objects.filter(categorygame__)
+
+            serializer = CommentSerializer(comment, context={'request': request})
+            return Response(serializer.data)
+
+        except Exception as ex:
+            return HttpResponseServerError(ex)
+
+
+    # # Get a list of all records
+    def list(self, request):
+        users = Comment.objects.all()
+
+
+        serializer = CommentSerializer(
+            users, many=True, context={'request': request})
+        return Response(serializer.data)
+
+
+    # # Edit a record via PUT method
+    def update(self, request, pk=None):
+        """Handle PUT requests for a game
+        Returns:
+            Response -- Empty body with 204 status code
+        """
+        # gamer = Gamer.objects.get(user=request.auth.user)
+        comment = Comment.objects.get(pk=pk)
+        comment.content = request.data["content"]
+        comment.created_on = request.data["created_on"]
+        comment.post = request.data["post"]
+        comment.author = request.data["author"]
+
+        comment.save()
+
+        return Response({}, status=status.HTTP_204_NO_CONTENT)
+
+
+    # DELETE a single record 
+    def destroy(self, request, pk=None):
+        """Handle DELETE requests for a single game
+        Returns:
+            Response -- 200, 404, or 500 status code
+        """
+        try:
+            user = Comment.objects.get(pk=pk)
+            user.delete()
+
+            return Response({}, status=status.HTTP_204_NO_CONTENT)
+
+        except Comment.DoesNotExist as ex:
+            return Response({'message': ex.args[0]}, status=status.HTTP_404_NOT_FOUND)
+
+        except Exception as ex:
+            return Response({'message': ex.args[0]}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+
+
+class CommentSerializer(serializers.ModelSerializer):   
+    class Meta:
+        model = Comment
+        fields = ('id', 'content', 'post', 'created_on', 'author')
+        depth = 1
