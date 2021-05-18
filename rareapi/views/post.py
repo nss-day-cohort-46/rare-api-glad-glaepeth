8c22d3d9409c61c0c1d1757c0ed7a43529dba746
Dylan Morris <dylanrobertmorris@gmail.com>
2021-05-18 14:28:38 -0500

    fix: nuked db remigrated reuploaded fixtures

--- rareapi/views/post.py
+++ rareapi/views/post.py
@@ -47,15 +47,49 @@ class PostView(ViewSet):
         except Exception as ex:
             return HttpResponseServerError(ex)
 
-    # def create(self, request):
-    #     post = Post()
-    #     category = Category.objects.get(pk=request.data["categoryId"])
-    #     post.category = category
-    #     post.
+    def create(self, request):
+        post = Post()
+        user = RareUser.objects.get(user=request.auth.user)
+        category = Category.objects.get(pk=request.data["category"])
+        post.user = user
+        post.category = category
+        post.title = request.data["title"]
+        post.publication_date = request.data["publication_date"]
+        post.image_url = request.data["image_url"]
+        post.content = request.data["content"]
+        post.approved = request.data["approved"]
 
+        try:
+            post.save()
+            serializer = PostSerializer(post, context={'request': request})
+            return Response(serializer.data)
+
+        except ValidationError as ex:
+            return Response({"reason": ex.message}, status=status.HTTP_400_BAD_REQUEST)
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['first_name', 'last_name', 'email']
+
+class PostUserSerializer(serializers.ModelSerializer):
+    user = UserSerializer(many=False)
+
+    class Meta:
+        model = RareUser
+        fields = ['user', 'bio']
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = ['label']
 
 
 class PostSerializer(serializers.ModelSerializer):
+    category = CategorySerializer(many=False)
+    user = PostUserSerializer(many=False)
+
     class Meta:
         model = Post
         fields = ('id', 'user', 'category', 'title', 'publication_date', 'image_url', 'content', 'approved')                
