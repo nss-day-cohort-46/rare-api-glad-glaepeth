93b46048e81c917ed562b8212cdc9c4cc68f3597
Ccourter11 <courtercalvin@yahoo.com>
2021-05-18 13:56:45 -0500

    feat: full crud in categories

--- rareapi/views/category.py
+++ rareapi/views/category.py
@@ -3,9 +3,23 @@ from rest_framework.viewsets import ViewSet
 from rest_framework.response import Response
 from rest_framework import serializers, status
 from rareapi.models import Category
+from django.core.exceptions import ValidationError
 
 class CategoryView(ViewSet):
+    def create(self, request):
 
+
+        category = Category()
+        category.label = request.data["label"]
+
+
+
+        try:
+            category.save()
+            serializer = CategorySerializer(category, context={'request': request})
+            return Response(serializer.data)
+        except ValidationError as ex:
+            return Response({"reason": ex.message}, status=status.HTTP_400_BAD_REQUEST)
 
     def retrieve(self, request, pk=None):
 
