d0f60cd4d35ede5f90406c8a82dcd8252d744b80
Ccourter11 <courtercalvin@yahoo.com>
2021-05-18 12:05:35 -0500

    feat: can see categories

---
+++ rareapi/views/category.py
@@ -0,0 +1,32 @@
+from django.http import HttpResponseServerError
+from rest_framework.viewsets import ViewSet
+from rest_framework.response import Response
+from rest_framework import serializers
+from rareapi.models import Category
+
+class CategoryView(ViewSet):
+
+
+    def retrieve(self, request, pk=None):
+
+        try:
+            category = Category.objects.get(pk=pk)
+            serializer = CategorySerializer(category, context={'request': request})
+            return Response(serializer.data)
+        except Exception as ex:
+            return HttpResponseServerError(ex)
+
+    def list(self, request):  
+        categories = Category.objects.all()
+
+
+        serializer = CategorySerializer(
+            categories, many=True, context={'request': request})
+        return Response(serializer.data)
+
+class CategorySerializer(serializers.ModelSerializer):
+# how we build the object
+
+    class Meta:
+        model = Category
+        fields = ('id', 'label')
