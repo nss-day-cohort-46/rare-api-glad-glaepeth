0f3780a275e9611d755d8b76522b9368ec2f3409
Caleb James <calebsjames@gmail.com>
2021-05-19 09:40:05 -0500

    fix: comment route changed to comments

--- rare/urls.py
+++ rare/urls.py
@@ -1,21 +1,5 @@
-"""rare URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path
-
 from rareapi.views import register_user, login_user
 from rareapi.views import RareUserView, TagView, CategoryView, CommentView
 
@@ -26,7 +10,7 @@ router = routers.DefaultRouter(trailing_slash=False)
 
 router.register(r'tags', TagView, "tags")
 router.register(r'users', RareUserView, 'user')
-router.register(r'comment', CommentView, 'comment')
+router.register(r'comments', CommentView, 'comment')
 router.register(r'categories', CategoryView, 'category')
 router.register(r'users', RareUserView, 'user')
 
